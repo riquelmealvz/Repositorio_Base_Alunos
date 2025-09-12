4f357a7463c321234d4c1c56bf0c2a086a6dfa17
riquelmealvz <riquelmeealvessxd@gmail.com>
2025-09-12 14:00:03 -0300

    Add files via upload

---
+++ Modulo 1/Listas/manipulacaodelista.py
@@ -0,0 +1,12 @@
+
+lista = list(range(1,11))
+print("Lista inicial:", lista)
+lista.append(11)
+
+lista.remove(5)
+lista.insert(2, 3)
+
+print("Lista final:", lista)
+print("Soma:", sum(lista))
+print("Maior:", max(lista))
+print("Menor:", min(lista))
